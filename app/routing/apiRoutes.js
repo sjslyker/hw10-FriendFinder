f85960bb65bffcb01a8984fffffaa825e2e4dc91
Steven S <stevenjslyker@gmail.com>
2019-03-24 21:58:23 -0600

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,25 @@
 var friendData = require("../data/friends");
 
-
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.json(friendData);
-      });
+      res.json(friendData);
+    });
 
-      app.post("/api/tables", function(req, res) {
-          friendData.push(req.body);
-          res.json(true);
-      });
+    app.post("/api/clear", function(req, res) {
+      // Empty out the arrays of data
+      friendData.length = 0;
+  
+      res.json({ ok: true });
+    });
 
-      app.post("/api/clear", function(req, res) {
-        // Empty out the arrays of data
-        friendData.length = 0;
-    
-        res.json({ ok: true });
-      });
+    app.post("/api/new-friends", function(req, res) {
+      // NEW CODE BELOW __________
+      var user = req.body;
+
+      for(var i = 0; i < user.scores.length; i++) {
+        user.scores[i] = parseInt(user.scores[i]);
+      }
+    });  
+
 };
